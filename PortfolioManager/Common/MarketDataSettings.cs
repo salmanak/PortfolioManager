6ab59cf464bd2994ab0334ab53d43fafd1053fa8
salmanak <salmanak@outlook.com>
2016-05-11 12:51:58 -0400

    Custom Market Data Settings Section

---
+++ PortfolioManager/Common/MarketDataSettings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Configuration;
+
+namespace PortfolioManager.Common
+{
+    public class MarketDataSettings : ConfigurationSection
+    {
+        public static MarketDataSettings GetConfiguration()
+        {
+            MarketDataSettings configuration =
+                ConfigurationManager
+                .GetSection("marketDataSettings")
+                as MarketDataSettings;
+
+            if (configuration != null)
+                return configuration;
+
+            return new MarketDataSettings();
+        }
+
+        [ConfigurationProperty("url", IsRequired = false)]
+        public string URL
+        {
+            get
+            {
+                return this["url"] as string;
+            }
+        }
+
+        [ConfigurationProperty("proxyAddress", IsRequired = false)]
+        public string ProxyAddress
+        {
+            get
+            {
+                return this["proxyAddress"] as string;
+            }
+        }
+    }
+
+}
