3b9d7caa142a624341744ba799e89391614caed3
salmanak <salmanak@outlook.com>
2016-05-22 00:12:06 -0400

    Moved ILogger interface in separate class file

--- PortfolioManager/Common/LoggingService.cs
+++ PortfolioManager/Common/LoggingService.cs
@@ -6,16 +6,6 @@ using System.Text;
 namespace PortfolioManager.Common
 {
     /// <summary>
-    /// Interface for other classes to log
-    /// </summary>
-    public interface ILogger
-    {
-        void Log(string message);
-        void LogDebug(string message);
-        void LogError(string message, Exception ex = null);
-    }
-
-    /// <summary>
     /// Wrapper Loggin Service class
     /// </summary>
     public class LoggingService:ILogger
