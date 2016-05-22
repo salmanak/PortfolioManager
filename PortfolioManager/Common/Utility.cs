f38d94b680a106adcf63f09ab3d4104e76036ccb
salmanak <salmanak@outlook.com>
2016-05-22 00:12:59 -0400

    Utility class for common tasks

---
+++ PortfolioManager/Common/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PortfolioManager.Common
+{
+    class Utility
+    {
+        public static double GetDouble(string valueString)
+        {
+            double valueDouble = 0;
+            double.TryParse(valueString, out valueDouble);
+            return valueDouble;
+        }
+    }
+}
